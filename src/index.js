fe89dbd582140d335a3914704243f6efc7bced67
Alexey Datskovskiy <alexdat2000@gmail.com>
2024-08-25 11:36:04 +0300

    Improve Clear and Reset buttons

--- src/index.js
+++ src/index.js
@@ -5,22 +5,26 @@ import "./style_main.css"
 import "./style_footer.css"
 import {todo_list} from "./todo-logic.js";
 import {StorageClear, StorageSetDefault} from "./storage";
-import {render} from "./dom-rendering";
+import {show_tab} from "./dom-rendering";
 
 window.onload = () => {
     const resetBtn = document.querySelector("#reset-btn");
     resetBtn.addEventListener("click", () => {
-        StorageSetDefault();
-        todo_list.Reload();
-        render();
+        if (window.confirm("Are you sure you want to reset storage?")) {
+            StorageSetDefault();
+            todo_list.Reload();
+            show_tab(0);
+        }
     });
 
     const clearBtn = document.querySelector("#clear-btn");
     clearBtn.addEventListener("click", () => {
-        StorageClear();
-        todo_list.Reload();
-        render();
+        if (window.confirm("Are you sure you want to clear storage?")) {
+            StorageClear();
+            todo_list.Reload();
+            show_tab(0);
+        }
     });
 
-    render();
+    show_tab(0);
 }
